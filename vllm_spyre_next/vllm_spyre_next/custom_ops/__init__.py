9ddf1db57f3bd9e65d8187280e1fd36b0725cc9f
Avishek Goswami <avishek.goswami@ibm.com>
2026-03-14 10:10:54 +0530

    VocabParallelEmbedding implementation for Spyre
    Signed-off-by: Avishek Goswami <avishek.goswami@ibm.com>

--- vllm_spyre_next/vllm_spyre_next/custom_ops/__init__.py
+++ vllm_spyre_next/vllm_spyre_next/custom_ops/__init__.py
@@ -1,6 +1,6 @@
 """This module contains all custom ops for spyre"""
 
-from . import rms_norm
+from . import rms_norm, vocab_parallel_embedding
 from vllm.logger import init_logger
 
 logger = init_logger(__name__)
@@ -9,3 +9,4 @@ logger = init_logger(__name__)
 def register_all():
     logger.info("Registering custom ops for spyre_next")
     rms_norm.register()
+    vocab_parallel_embedding.register()
